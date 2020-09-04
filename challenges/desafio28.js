cf3470f70cab1fec2a73175f587fa10403086c4f
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-09-04 19:36:37 -0300

    cc

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,9 +1,9 @@
 db.resumoVoos.insertOne({
-  empresa: "LATAM AIRLINES BRASIL",
   totalVoosDomesticos: db.voos.count({
     "empresa.nome": "LATAM AIRLINES BRASIL",
     natureza: "Doméstica"
-  })
+  }),
+  empresa: "LATAM AIRLINES BRASIL",
 });
 
 db.resumoVoos.findOne({ empresa: "LATAM AIRLINES BRASIL" }, { _id: 0 }).pretty();
