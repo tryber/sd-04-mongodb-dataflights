6fbbef9c23aeb7f114cc9dbe0e15f1bc0dd54959
igorgbr <igorgphotoarte@gmail.com>
2020-09-05 16:49:04 -0300

    ate o 24

---
+++ challenges/desafio22.js
@@ -0,0 +1,19 @@
+db.voos.find(
+    {
+      $and: [
+        {
+          $or: [
+            { "empresa.nome": "DELTA AIRLINES" },
+            { "empresa.nome": "AMERICAN AIRLINES" },
+          ],
+        },
+        {
+            $and: [
+                {"aeroportoOrigem.sigla": "SBGR"},
+                {"aeroportoDestino.sigla": "KJFK"}
+            ]
+        }
+      ],
+    },
+    { vooId: 1, _id: 0 }
+  ).limit(1);
---
+++ challenges/desafio24.js
@@ -0,0 +1,21 @@
+db.voos
+  .find(
+    {
+      $and: [
+        {
+          $or: [
+            { "empresa.nome": { $ne: "GOL" } },
+            { "empresa.nome": { $ne: "AZUL" } },
+          ],
+        },
+        {
+          $and: [
+            { litrosCombustivel: { $lt: 600 } },
+            { litrosCombustivel: { $exists: true } },
+          ],
+        },
+      ],
+    },
+    { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 }
+  )
+  .limit(1);
