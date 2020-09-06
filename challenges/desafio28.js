1f9fe64735283ecda5fd38235d1697d36310d044
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-05 22:04:44 -0300

    tirei as aspas de uma das chaves.

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,7 +1,7 @@
 db.resumoVoos.insertOne({
   "empresa": "LATAM AIRLINES BRASIL",
   "totalVoosDomesticos": db.voos.count({
-    "natureza": "Doméstica",
+    natureza: "Doméstica",
     "empresa.nome": "LATAM AIRLINES BRASIL"
 
   }),
