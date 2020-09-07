68f20d229218e02fa1fc83f856e0af57141cb963
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-09-07 18:04:14 -0300

    cc

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,1 +1,3 @@
-db.voos.find({"rtk": {$exists: false}}, {"_id": 0, "vooId": 1}).limit(1);
+db.voos.find({"rtk": {$exists: false}},
+{"_id": 0, "vooId": 1})
+.limit(1);
--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,1 +1,3 @@
-db.voos.find({"vooId": { $eq: 756807}}, {"_id": 0, "empresa.sigla": 1,"empresa.nome": 1,"passageiros": 1 }).pretty();
+db.voos.find({"vooId": { $eq: 756807}},
+{"_id": 0, "empresa.sigla": 1,"empresa.nome": 1,"passageiros": 1 })
+.pretty();
