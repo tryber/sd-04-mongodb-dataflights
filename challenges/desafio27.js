ce804e7d6359ed5f15f3637710543cc28af42102
henrique <hbaetaleite@gmail.com>
2020-09-07 19:33:02 -0300

    faltou o .count()

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -12,6 +12,6 @@
 
 //db.voos.find({ "empresa.nome": "PASSAREDO"}, {"natureza": "Doméstica"})
 
-db.resumoVoos.insert({"empresa": "PASSAREDO", "totalVoosDomesticos": db.voos.find({ "empresa.nome": "PASSAREDO"}, {"natureza": "Doméstica"}) } );
+db.resumoVoos.insert({"empresa": "PASSAREDO", "totalVoosDomesticos": db.voos.find({ "empresa.nome": "PASSAREDO"}, {"natureza": "Doméstica"}).count() } );
 
 db.resumoVoos.findOne({"empresa": "PASSAREDO"}, {"_id": 0});
