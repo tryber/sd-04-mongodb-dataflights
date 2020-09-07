785574763fdf58a859cab57c64e49a9702ba6a88
henrique <hbaetaleite@gmail.com>
2020-09-07 19:21:55 -0300

    codeclimate v2

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -10,6 +10,6 @@
 
 // db.voos.count({ $and: [{ "empresa.nome": "PASSAREDO"}, {"natureza": "Doméstica"}] });
 
-db.resumoVoos.insertOne({"empresa": "PASSAREDO", "totalVoosDomesticos": db.voos.count( { $and: [ { "empresa.nome": "PASSAREDO"}, {"natureza": "Doméstica"} ] } ) } );
+db.resumoVoos.insert({"empresa": "PASSAREDO", "totalVoosDomesticos": db.voos.count( { $and: [ { "empresa.nome": "PASSAREDO"}, {"natureza": "Doméstica"} ] } ) } );
 
 db.resumoVoos.findOne({"empresa": "PASSAREDO"}, {"_id": 0});
