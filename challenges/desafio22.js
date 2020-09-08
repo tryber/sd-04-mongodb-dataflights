9348998894cf82547537f43e91fcf97cf0dd935f
Lucas Ribas <lucas@mundoribas.com.br>
2020-09-08 19:33:21 -0300

    Code Climate 2X0

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -10,3 +10,13 @@ db.voos
     { vooId: 1, _id: 0 }
   )
   .limit(1);
+
+
+  // db.voos.findOne(
+  //   {
+  //     "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+  //     "aeroportoOrigem.sigla": "SBGR",
+  //     "aeroportoDestino.sigla": "KJFK",
+  //   },
+  //   { vooId: 1, _id: 0 }
+  // );
