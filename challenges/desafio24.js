1d7e6b7b9575be3c3299cb7a69dc67ea1e9d057e
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-09-08 19:46:22 -0300

    teste 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -2,9 +2,9 @@ db.voos
   .find(
     {
       $and: [
+        { $or: [{ "empresa.nome": /GOL/ }, { "empresa.nome": /AZUL/ }] },
         { litrosCombustivel: { $lt: 600 } },
         { litrosCombustivel: { $exists: true } },
-        { $or: [{ "empresa.nome": "GOL" }, { "empresa.nome": "AZUL" }] },
       ],
     },
     { _id: 0, "empresa.nome": 1, litrosCombustivel: 1 }
