90d0b7befb62e4d8c4d7e31110435bf175b3fb2a
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-09-08 17:52:54 -0300

    22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,5 +1,5 @@
-db.voos.findOne({ $and: [
-  {"empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"]}},
-  {"aeroportOrigem.sigla": { $eq: "SBGR" }},
-  {"aeroportoDestino.sigla": { $eq: "KJFK" }}
-]},{vooId: 1, _id: 0})
+db.voos.find({
+  "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"]},
+  "aeroportOrigem.sigla": "SBGR" ,
+  "aeroportoDestino.sigla": "KJFK" 
+},{vooId: 1, _id: 0}).limit(1);
