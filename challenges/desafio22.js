8d07211a99246a64a7449743384096dc0e625504
Ronald Lima <ronaldazevedolima@gmail.com>
2020-09-08 19:35:51 -0300

    Eu fico louco, mas um sorriso me faz entender, saber por

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,1 @@
-db.voos.find({$and: [{$or:[{"empresa.nome": "DELTA AIRLINES"},{"empresa.nome": "AMERICAN AIRLINES"}]},{"aeroportoOrigem.sigla": "SBGR"},{"aeroportoDestino.sigla": "KJFK"}]},{"vooId": 1}).sort({"empresa.nome": 1}).limit(1);
+db.voos.find({$and: [{$or:[{"empresa.nome": "DELTA AIRLINES"},{"empresa.nome": "AMERICAN AIRLINES"}]},{"aeroportoOrigem.sigla": "SBGR"},{"aeroportoDestino.sigla": "KJFK"}]},{"vooId": 1, _id: 0}).sort({"empresa.nome": 1}).limit(1);
