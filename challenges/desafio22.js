5bcea89d0b9e353479d1e2ef3f6f01f2353d0391
Lucas Ribas <lucas@mundoribas.com.br>
2020-09-08 19:23:52 -0300

    Desafios até o 23

---
+++ challenges/desafio22.js
@@ -0,0 +1,12 @@
+db.voos
+  .find(
+    {
+      $and: [
+        { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] } },
+        { "aeroportoDestino.sigla": "KJFK" },
+        { "aeroportoOrigem.sigla": "SBGR" },
+      ],
+    },
+    { vooId: 1, _id: 0 }
+  )
+  .limit(1);
