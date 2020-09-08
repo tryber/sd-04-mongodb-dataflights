b1baa7ad9beb48f792c12339a4eeb85b852b5305
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-09-08 11:58:58 -0300

    corrigindo req 10 e cumprindo req 15 ao 18

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,4 +2,12 @@
 // Exiba apenas os campos vooId, empresa.nome, aeroportoOrigem.nome, aeroportoDestino.nome, mes e ano
 
 db.voos.find({"empresa.nome": "GOL", "ano":2017 },
-{"_id":0, "vooId":1,"mes" : 1, "aeroportoOrigem.nome":1, "aeroportoDestino.nome":1, }).limit(10).pretty();
+{
+"_id": 0,
+"vooId": 1,
+"empresa.nome": 1,
+"aeroportoOrigem.nome": 1,
+"aeroportoDestino.nome": 1,
+"mes": 1,
+"ano": 1
+}).limit(10).pretty();
