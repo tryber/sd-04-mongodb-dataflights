460e36aee3c37d3031beca0aa54486f7e83d800e
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-09-08 18:29:21 -0300

    desafio27

---
+++ challenges/desafio27.js
@@ -0,0 +1,3 @@
+db.voos.find({"empresa.nome": "PASSAREDO", "natureza": "Doméstica"}).count();
+db.resumoVoos.insertOne({"empresa": "PASSAREDO", "totalVoosDomesticos": 4187 });
+db.resumoVoos.findOne({"empresa": { $eq: "PASSAREDO"}}, {_id: 0, "empresa": 1, "totalVoosDomesticos": 1});
