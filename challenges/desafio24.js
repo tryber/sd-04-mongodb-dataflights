9f7df52ed46c6f9abe34794f6f43b5f634d0f5eb
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-09-08 17:33:17 -0300

    desafio25

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,4 +1,4 @@
-db.voos.findOne({$and: [{"litrosCombustivel": {$lte: 600}},
+db.voos.findOne({$and: [{$and: [{"litrosCombustivel": {$lte: 600}},
 {$nor: [{"empresa.nome": "GOL"},{"empresa.nome": "AZUL"}]}
-]}, 
+]}, {"litrosCombustivel": {$exists: true}}]}, 
 {"_id": 0, "vooId": 1,"empresa.nome": 1, "litrosCombustivel": 1});
