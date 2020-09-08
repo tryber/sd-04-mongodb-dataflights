35e284b701e92770ec6cd61e620a8b46bfa6bacd
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-09-08 19:02:43 -0300

    dunno

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,5 +1,5 @@
 db.voos.find({
-  "empresa.nome": { $or: ["DELTA AIRLINES", "AMERICAN AIRLINES"]},
+  "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"]},
   "aeroportoOrigem.sigla": "SBGR",
   "aeroportoDestino.sigla": "KJFK"
 }, { _id: 0, vooId: 1 }).limit(1);
