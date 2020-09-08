a6cb27c759ca2dbb4386132d851661235ee08781
Tiago-ers <trsilva284@gmail.com>
2020-09-08 20:40:30 -0300

    cc

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -5,8 +5,8 @@ db.voos
         { "litrosCombustivel": { $lte: 600 } },
         {"litrosCombustivel": {$exists: true}}
       ],
-       "empresa.nome": { $ne: "GOL" },
-       "empresa.nome": { $ne: "AZUL" },
+        "empresa.nome": { $ne: "GOL" },
+        "empresa.nome": { $ne: "AZUL" },
 
     },
     { "_id": 0, "vooId": 1, "empresa.nome": 1, "litrosCombustivel": 1 }
--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,5 +1,5 @@
 db.voos
   .deleteMany({
     $and: [{"passageiros.pagos": {$gte: 5}}, {"passageiros.pagos": {$lte: 10}}],
-     "empresa.nome": "GOL" 
+      "empresa.nome": "GOL" 
   });
