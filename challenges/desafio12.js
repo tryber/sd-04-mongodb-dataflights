06aa1052dbd2f9bc2249318d31b4e6edfcd88ab8
procopio420 <lucas-procopio@outlook.com>
2020-09-08 19:38:23 -0300

    Corrected mistakes

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,1 +1,1 @@
-db.voos.find({$or: [{"aeroportoDestino.pais": "BRASIL"}, {"aeroportoDestino.pais": "ARGENTINA"}, {"aeroportoDestino.pais": "CHILE"}]}).count();
+db.voos.find({ $or: [{ "aeroportoDestino.pais": "BRASIL" }, { "aeroportoDestino.pais": "ARGENTINA" }, { "aeroportoDestino.pais": "CHILE" }] }).count();
