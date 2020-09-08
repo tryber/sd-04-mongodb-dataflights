3b7ca39f1095ccd4869765a8454c9e8b528e6ece
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-09-08 12:13:04 -0300

    até o requisito 21

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -1,1 +1,2 @@
 // Retorne o vooId do primeiro voo em que o campo litrosCombustivel exista
+db.voos.findOne({ "litrosCombustivel": { $exists: 1 } }, { "_id": 0, "vooId": 1 });
--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,1 +1,3 @@
 // Retorne o vooId do primeiro voo em que o campo rtk não exista
+db.voos.findOne({ "rtk": { $exists: 0 } }, { "_id": 0, "vooId": 1 });
+
