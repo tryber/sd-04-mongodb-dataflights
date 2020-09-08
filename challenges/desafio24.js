35bdaf95a8fbf86b4b5778dea2f51b627563438c
leandromarten1 <leandromarten1@gmail.com>
2020-09-08 18:00:22 -0300

    Fix CC 1

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -10,4 +10,3 @@ db.voos
     { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }
   )
   .limit(1);
-  
--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,4 +1,3 @@
 // Erro TypeError: x.toString is not a function
 db.resumoVoos.insertOne({"empresa": "PASSAREDO", "totalVoosDomesticos": db.voos.find({"empresa.nome": "PASSAREDO", "natureza": "Doméstica"})});
 db.resumoVoos.find({empresa: "PASSAREDO"}, {_id: 0}); // Aqui
-
