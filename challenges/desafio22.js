5c561bcecce15142aa6572747d66a719841b9072
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-09-08 19:14:04 -0300

    Teste 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,21 @@
+db.voos
+  .find(
+    {
+      $and: [
+        {
+          $or: [
+            { "empresa.nome": "DELTA AIRLINES" },
+            { "empresa.nome": "AMERICAN AIRLINES" },
+          ],
+        },
+        {
+          $or: [
+            { "aeroportoOrigem.sigla": "SBGR" },
+            { "aeroportoDestino.sigla": "KJFK" },
+          ],
+        },
+      ],
+    },
+    { vooId: 1, _id: 0 }
+  )
+  .limit(1);
