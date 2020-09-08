db89859330f241e3ccc18c92138a24e6e5171173
Julio Cesar <stininmatic@hotmail.com>
2020-09-08 20:42:05 -0300

    Dreams money can't buy

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,3 @@
-db.voos.findOne({$and: [{$or: [{"empresa.nome": "DELTA AIRLINES"}, {"empresa.nome": "AMERICAN AIRLINES"}]},{"aeroportoOrigem.sigla": "SBGR"}, {"aeroportoDestino.sigla": "KJFK"}]},{"vooId": 1, "_id": 0});
+db.voos.findOne({$and: [{$or: [{"empresa.nome": "DELTA AIRLINES"},
+{"empresa.nome": "AMERICAN AIRLINES"}]},{"aeroportoOrigem.sigla": "SBGR"},
+{"aeroportoDestino.sigla": "KJFK"}]},{"vooId": 1, "_id": 0});
