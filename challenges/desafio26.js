1c4cdfac80432f1f0cba095354c365ea08287e9f
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-09-08 19:07:09 -0300

    reqs 25 e 26

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,3 +1,5 @@
 // Remova todos os voos da empresa GOL em que a quantidade de passageiros pagos
 // esteja entre 5 e 10, incluindo os casos em que a quantidade é 5 e 10.
 // Informe a quantidade de documentos removidos.
+db.voos.deleteMany({"empresa.nome": "GOL", $and: [{"passageiros.pagos": {$gte: 5}}, {"passageiros.pagos": {$lte: 10}}]});
+// { "acknowledged" : true, "deletedCount" : 74 }
