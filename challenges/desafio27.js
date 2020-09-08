80b13d5f4ff53dfec3048d2d62f0007605b47c21
matheusysd <matheusyurisd@gmail.com>
2020-09-08 16:31:29 -0300

    :100:

---
+++ challenges/desafio27.js
@@ -0,0 +1,5 @@
+TOTAL_VOOS_PASSAREDO = db.voos.find({"natureza":"Doméstica", "empresa.nome":"PASSAREDO"}).count();
+
+db.resumoVoos.insertOne({"empresa":"PASSAREDO", "totalVoosDomesticos": TOTAL_VOOS_PASSAREDO});
+
+db.resumoVoos.findOne({"empresa":"PASSAREDO"},{"_id":0});
