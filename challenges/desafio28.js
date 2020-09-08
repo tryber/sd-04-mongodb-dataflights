3198d812b432aede3f28a10b1b253adf9b42c34b
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-09-08 18:51:10 -0300

    cc

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,2 +1,2 @@
-db.resumoVoos.insertOne({"empresa": "LATAM AIRLINES BRASIL", "totalVoosDomesticos": db.voos.find({"empresa.nome": "LATAM AIRLINES BRASIL", "natureza": "Doméstica"}).count() });
-db.resumoVoos.findOne({"empresa": { $eq: "LATAM AIRLINES BRASIL"}}, {_id: 0, "empresa": 1, "totalVoosDomesticos": 1});
+db.resumoVoos.insertOne({empresa: "LATAM AIRLINES BRASIL", "totalVoosDomesticos": db.voos.find({"empresa.nome": "LATAM AIRLINES BRASIL", "natureza": "Doméstica"}).count() });
+db.resumoVoos.findOne({empresa: { $eq: "LATAM AIRLINES BRASIL"}}, {_id: 0, "empresa": 1, "totalVoosDomesticos": 1});
