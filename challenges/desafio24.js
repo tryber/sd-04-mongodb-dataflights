3b778e2c76da0796cffe6ab15212999c42a867a6
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-09-08 19:47:02 -0300

    FIX 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -7,6 +7,6 @@ db.voos
         { litrosCombustivel: { $exists: true } },
       ],
     },
-    { _id: 0, "empresa.nome": 1, litrosCombustivel: 1 }
+    { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }
   )
   .limit(1);
