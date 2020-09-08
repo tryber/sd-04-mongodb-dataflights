823b394eda8aeb2575d25c14cc424035e0a1b21c
Josias Viveiro <josiasviveiro@gmail.com>
2020-09-08 16:56:47 -0300

    desafio 14 corrigido

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -2,4 +2,4 @@
 Desafio 14
 Retorne o total de voos em que o país de origem não seja BRASIL.
 */
-db.voos.count( { "aeroportoOrigem.nome": { $not: { $eq: "BRASIL"} } });
+db.voos.count( { "aeroportoOrigem.nome": { $ne: "BRASIL"} });
