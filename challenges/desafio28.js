10de42a52baed252cad6395beed6d83609ae7d1c
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-09-08 17:22:25 -0300

    Desafios 25, 25, 27 e 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,24 @@
+// Retorne a quantidade total de voos de natureza Doméstica que a empresa LATAM AIRLINES BRASIL possui, via uso de uma nova coleção chamada resumoVoos.
+
+// Para isso, escreva no arquivo desafio28.js duas queries, nesta ordem:
+
+// Conte quantos voos da empresa cujo campo natureza possua valor igual a Doméstica e crie uma query que insira na coleção resumoVoos um documento com os campos: empresa (nome da empresa) e totalVoosDomesticos (o total retornado anteriormente).
+
+// Em uma segunda query, retorne a empresa e o totalVoosDomesticos do primeiro documento presente na coleção resumoVoos em que a empresa seja LATAM AIRLINES BRASIL.
+
+total = db.voos.count({
+  "empresa.nome": "LATAM AIRLINES BRASIL",
+  natureza: "Doméstica",
+});
+
+db.resumoVoos.insert({
+  empresa: "LATAM AIRLINES BRASIL",
+  totalVoosDomesticos: total,
+});
+
+db.resumoVoos
+  .find(
+    { empresa: "LATAM AIRLINES BRASIL" },
+    { empresa: 1, totalVoosDomesticos: 1, _id: 0 }
+  )
+  .limit(1);
