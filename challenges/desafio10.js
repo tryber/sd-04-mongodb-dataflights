6ad9d5885b00f73cbd35433d7690b246eb5cd3f7
lourenco <loulage@gmail.com>
2020-09-10 18:02:58 -0300

    fix desafio 10 e feito desafio 9

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,1 +1,17 @@
-retty();db.voos.find({"ano": 2017, "empresa.nome": "GOL"}, {"vooId": 1, "empresa.nome": 1, "aeroportoOrigem.nome": 1, "aeroportoDestino.nome": 1, "mes": 1, "ano": 1, "_id": 0}).limit(10).p
+db.voos.find(
+    {
+      $and: [
+        {"empresa.nome": "GOL"},
+        {ano: 2017}
+      ]
+    },
+    {
+      vooId: 1,
+      "empresa.nome": 1,
+      _id: 0,
+      "aeroportoOrigem.nome": 1,
+      "aeroportoDestino.nome": 1,
+      mes: 1,
+      ano: 1
+    }
+  ).limit(10).pretty();
