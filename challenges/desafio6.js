6aece239d518620072a34f9a8f14686a8c448a91
Kaecio <kaecio@yahoo.com.br>
2020-09-11 20:43:34 -0300

    Desafio 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,4 @@
+db.voos.find({"vooId":756807},{"_id":0,"empresa.nome":1,"empresa.sigla":1,"passageiros.pagos":1,"passageiros.gratis":1}).pretty()
+/* Desafio 6
+Retorne apenas os campos empresa.sigla, 
+empresa.nome e passageiros do voo com o campo vooId igual a 75807.*/
