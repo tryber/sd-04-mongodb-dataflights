0c2c53b787326bc0a121c6e45298d9a11591ffe1
Kaecio <kaecio@yahoo.com.br>
2020-09-11 21:53:07 -0300

    Desafio 5, sem querer joguei o req6 no arq do req5, corrigido

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,4 +1,3 @@
 db.voos.find({}, {"_id":0,"vooId": 1}).limit(3).skip(9)
-db.voos.findOne({"vooId":756807},{"_id":0,"empresa.nome":1,"empresa.sigla":1},{"passageiros.pagos":1})
 /*Desafio 5
 Retorne o vooId do décimo ao décimo segundo documento da coleção voos. */
