e44ce610d5c8e7e1c30756144ce8cc749897e5b8
Pedro <pedrotpoliveira@gmail.com>
2020-09-12 04:17:34 -0300

    Desafio 28 - CC Fix

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,9 +1,11 @@
+TOTAL_LATAM = db.voos.find({
+  "empresa.nome": "LATAM AIRLINES BRASIL",
+  "natureza": "Doméstica"
+}).count();
+
 db.resumoVoos.insertOne({
   "empresa": "LATAM AIRLINES BRASIL",
-  "totalVoosDomesticos": db.voos.find({
-    "empresa.nome": "LATAM AIRLINES BRASIL",
-    "natureza": "Doméstica"
-  }).count()
+  "totalVoosDomesticos": TOTAl_LATAM
 });
 
 db.resumoVoos.findOne({ "empresa": "LATAM AIRLINES BRASIL" }, { "_id": 0 });
