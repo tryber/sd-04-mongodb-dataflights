315a2d7bf9fa6a876ad7cbd837e6bf30a23d9b01
danilossousa <danilosantossousa@gmail.com>
2020-09-12 05:33:59 -0300

    desafios25, 26, 27 e 28

---
+++ challenges/desafio28.js
@@ -0,0 +1,5 @@
+db.voos.count({$and: [{"empresa.nome": "LATAM AIRLINES BRASIL"}, {"natureza": "Doméstica"}]});
+
+db.resumoVoos.insertOne({"empresa": "LATAM AIRLINES BRASIL", "totalVoosDomesticos": 20026});
+
+db.resumoVoos.findOne({"empresa": "LATAM AIRLINES BRASIL"}, {"_id": 0, "empresa": 1, "totalVoosDomesticos": 1});
