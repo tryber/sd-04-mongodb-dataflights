70f993433bf6ca0d1d186384edb1345d0557fa61
Thiago Tavares <thiagooo15@gmail.com>
2020-09-14 17:40:40 -0300

    preparar o desembarque

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,3 +1,10 @@
-// Retorne o vooId do primeiro voo em que a empresa seja DELTA AIRLINES ou AMERICAN AIRLINES, 
+// Retorne o vooId do primeiro voo em que a empresa seja DELTA AIRLINES ou AMERICAN AIRLINES,
 // a sigla do aeroporto de origem seja SBGR e a sigla do aeroporto de destino seja KJFK.
-db.voos
+db.voos.findOne(
+  {
+    "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+    "aeroportoOrigem.sigla": "SBGR",
+    "aeroportoDestino.sigla": "KJFK",
+  },
+  { _id: 0, vooId: 1 }
+);
--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,3 +1,11 @@
 // Retorne o vooId e litrosCombustivel do primeiro voo em que o campo litrosCombustivel não seja maior
 // do que 1000 e o campo litrosCombustivel exista.
-db.voos
+db.voos.findOne(
+  {
+    $and: [
+      { litrosCombustivel: { $lte: 1000 } },
+      { litrosCombustivel: { $exists: true } },
+    ],
+  },
+  { _id: 0, vooId: 1, litrosCombustivel: 1 }
+);
