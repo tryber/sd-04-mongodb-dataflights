0d57fd65be812b54f1c25dc3756ef32760d61650
Victor L <victor.lustosacp@gmail.com>
2020-09-14 17:49:13 -0300

    24

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,4 +1,4 @@
-db.voos.find({"empresa.nome": "AZUL"}).limit(10);
+db.voos.find({"empresa.nome": "AZUL"}).limit(10).pretty();
 // procura os 10 primeiros voos da empresa com nome azul
 // nao consegui fzer funcionar usando anihamneto
 // { 'empresa' : { 'nome' : 'azul } }....
