357ee3e4df126900f36c8e39468f7abe0137f488
wistyk <leonardoxw@gmail.com>
2020-09-15 13:02:35 -0300

    she done already done had herses

---
+++ challenges/desafio22.js
@@ -0,0 +1,1 @@
+db.voos.find( { $or: [ { "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" } ], "aeroportoDestino.sigla": "KJFK", "aeroportoOrigem.sigla": "SBGR" }, {"_id":0, "vooId":1}).limit(1);
