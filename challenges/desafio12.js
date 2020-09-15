f36ce8364e3eaea01f4b0b9567507f9272af31d2
Kaecio <kaecio@yahoo.com.br>
2020-09-14 22:03:47 -0300

    Desafio 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,9 @@
+db.voos.find({"aeroportoDestino.pais":"BRASIL"},{"aeroportoDestino.pais":"ARGENTINA"},{"aeroportoDestino.pais":"CHILE"}).count()
+
+db.voos.find(
+  {
+    $or:[{"aeroportoDestino.pais":"BRASIL"},{"aeroportoDestino.pais":"ARGENTINA"},{"aeroportoDestino.pais":"CHILE"}]
+  }
+  ).count()
+/*Desafio 12
+Conte os documentos em que o campo aeroportoDestino.pais seja igual a BRASIL, ARGENTINA ou CHILE.*/
