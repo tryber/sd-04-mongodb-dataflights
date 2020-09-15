e0fda6227c56db7c39afa8f8ec400fe9889960c7
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-09-15 10:15:31 -0300

    trying this pain in my test

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,10 +1,5 @@
-// const flights = db.voos.find({"empresa.nome": "PASSAREDO", "natureza": "Doméstica"}).count();
-// db.resumoVoos.insert({ "empresa": "PASSAREDO", "totalVoosDomesticos": flights });
-
-db.resumoVoos.insertOne({ 
-  "empresa": "PASSAREDO", 
-  "totalVoosDomesticos": db.voos.find({"empresa.nome": "PASSAREDO", "natureza": "Doméstica"}).count()
-});
+const flights = db.voos.find({"empresa.nome": "PASSAREDO", "natureza": "Doméstica"}).count();
+db.resumoVoos.insert({ "empresa": "PASSAREDO", "totalVoosDomesticos": flights });
 
 db.resumoVoos.find({"empresa": "PASSAREDO"}, 
 {"_id": false, "empresa": true, "totalVoosDomesticos": true});
