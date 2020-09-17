44b7fd75fa90dcc9b295bc756f854ca047938207
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-09-17 16:50:29 -0300

    14 to 20

---
+++ challenges/desafio13.js
@@ -0,0 +1,3 @@
+// Desafio 13
+// Conte os documentos em que o campo aeroportoDestino.continente não seja igual a EUROPA, ÁSIA e OCEANIA.
+db.voos.find({"aeroportoDestino.continente": { $nin: ["EUROPA", "ÁSIA", "OCEANIA"] }}).count();
