b3dca644a07fb6554aadc558f209d69c33b6d001
saturnian99 <saturnian.dsg@gmail.com>
2020-09-21 14:17:48 -0300

    Fixing Desafio 27

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,3 +1,5 @@
-const result = db.voos.count({ natureza: "Doméstica", "empresa.nome": "PASSAREDO" });
-db.resumoVoos.insertOne({ empresa: "PASSAREDO", totalVoosDomésticos: result });
-db.resumoVoos.findOne({ "empresa": "PASSAREDO" }, { empresa: 1, totalVoosDomesticos: 1 });
+db.resumoVoos.insertOne({
+  "empresa": "PASSAREDO",
+  "totalVoosDomesticos": db.voos.count({"empresa.nome": "PASSAREDO", "natureza": "Doméstica"})
+});
+db.resumoVoos.findOne({"empresa": "PASSAREDO"}, {empresa: 1, totalVoosDomesticos: 1, _id: 0});
