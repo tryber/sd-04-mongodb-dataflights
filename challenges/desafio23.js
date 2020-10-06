6d57bbf0d568a3c95b36c498e03188d3a416f44e
binhus <eng.robsonwilli@gmail.com>
2020-10-06 16:59:38 -0300

    desafio 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,9 @@
+db.voos.findOne(
+  {
+    $and: [
+      { litrosCombustivel: { $lte: 1000 } },
+      { litrosCombustivel: { $exists: true } },
+    ],
+  },
+  { _id: false, vooId: true, litrosCombustivel: true }
+);
