d511c652ff648b537ab81b061ff0ed32c5c809c6
binhus <eng.robsonwilli@gmail.com>
2020-10-05 21:17:38 -0300

    desafio 22 e corrigindo arquivo 12

---
+++ challenges/desafio22.js
@@ -0,0 +1,1 @@
+db.voos.findOne({$and: [{$or: [{"empresa.nome": "DELTA AIRLINES"}, {"empresa.nome": "AMERICAN AIRLINES"}]}, {"aeroportoDestino.sigla": "KJFK"}, {"aeroportoOrigem.sigla": "SBGR"}]}, {"vooId": 1, "_id": 0});
