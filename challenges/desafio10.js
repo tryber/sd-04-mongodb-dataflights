4c9b915f0482007070c2f7e3a255fb0a1ad03b6b
binhus <eng.robsonwilli@gmail.com>
2020-10-07 15:03:53 -0300

    aparando as arestas

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,1 +1,1 @@
-db.voos.find({"ano": 2017, "empresa.nome": "AZUL"}, { vooId: true, "empresa.nome": true, "aeroportoOrigem.nome": true, "aeroportoDestino.nome": true, mes: true, ano: true, _id:false  }).limit(10);
+db.voos.find({"ano": 2017, "empresa.nome": "GOL"}, { "vooId": true, "empresa.nome": true, "aeroportoOrigem.nome": true, "aeroportoDestino.nome": true, "mes": true, "ano": true, "_id":false  }).limit(10);
--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,1 +1,9 @@
-db.voos.find({"aeroportoDestino.pais": {$in: ["BRASIL","ARGENTINA","CHILE"]}).count();
+db.voos
+  .find({
+    $or: [
+      { "aeroportoDestino.pais": "BRASIL" },
+      { "aeroportoDestino.pais": "ARGENTINA" },
+      { "aeroportoDestino.pais": "CHILE" },
+    ],
+  })
+  .count();
